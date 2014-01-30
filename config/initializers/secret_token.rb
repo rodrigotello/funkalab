b9083e8f981d115475882073eba8b5204bdf89f7
rodrigotello <rodrigotello@outlook.com>
2014-01-30 14:51:45 -0600

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Funkalab::Application.config.secret_token = '9cb64ac1e20eee1e9586dc6be5fe7fa60d7a968aaa0b96eb867876410d5cc7d5b81524816b861c0067478904a46987abf8bc9bb435ae32bcce5e0cdc37da7f44'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+Funkalab::Application.config.session_store :cookie_store, key: '_funkalab_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rails generate session_migration")
+# Funkalab::Application.config.session_store :active_record_store
